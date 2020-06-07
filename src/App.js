2d0d9b82bebb35083810b9f217c63cbaf9035921
william <william.dobson@sykescottages.co.uk>
2020-06-07 21:27:38 +0100

    add tailwind css to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import Note from "./components/Note";
 
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './styles/tailwind.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
