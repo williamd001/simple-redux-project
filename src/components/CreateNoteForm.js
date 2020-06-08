8931ca2908f56eb0b3998eb28cec5af2f9a410df
william <william.dobson@sykescottages.co.uk>
2020-06-08 14:04:22 +0100

    add form change listeners

--- src/components/CreateNoteForm.js
+++ src/components/CreateNoteForm.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function CreateNoteForm() {
 
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        //todo dispatch react
+
+        setTitle('');
+        setBody('');
+    }
 
     return (
-            <form className="w-full max-w-md mx-auto shadow mt-4 p-6">
+        <form className="w-full max-w-md mx-auto shadow mt-4 p-6" onSubmit={handleSubmit}>
 
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                    Title
-                </label>
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                Title
+            </label>
 
-                <input type="text" name="title" id="title"
-                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
-                />
+            <input type="text" name="title" id="title"
+                   onChange={event => setTitle(event.target.value)}
+                   value={title}
+                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+            />
 
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="note">
-                    note body
-                </label>
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="note">
+                note body
+            </label>
 
-                <textarea name="body" id="note"
-                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-3"
-                          rows="5"
+            <textarea name="body" id="note"
+                      onChange={event => setBody(event.target.value)}
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-3"
+                      rows="5"
+                      value={body}
+            />
 
-                />
-
-                <input type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"/>
-            </form>
+            <input type="submit"
+                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"/>
+        </form>
     );
 }
 
